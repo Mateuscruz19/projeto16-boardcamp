cb28a4239fd1702ba6e3f27c68527b5204e9d078
Mateuscruz19 <profissionalmateuscruz@gmail.com>
2023-02-12 10:24:29 -0300

    Refatoreed

--- src/controllers/rentalsCONTROLLER.js
+++ src/controllers/rentalsCONTROLLER.js
@@ -57,47 +57,31 @@ export async function findAll(req, res) {
 }
 
 export async function returnGame(req, res) {
-  const { id } = req.params;
 
-  try {
-    const result = await connectionDB.query(
-      "SELECT * FROM rentals WHERE id=$1",
-      [id]
-    );
+  const { id } = req.params
 
-    const rental = result.rows[0];
+    try {
+        const returnDate = new Date(Date.now())
 
-    if (result.rowCount === 0) return res.sendStatus(404);
-    if (rental.returnDate) return res.sendStatus(400);
+        const { rentDate, daysRented, gameId } = req.rental.rows[0]
 
-    const diffInTime =
-      new Date().getTime() - new Date(rental.rentDate).getTime();
-    const diffInDays = Math.floor(diffInTime / (24 * 3600 * 1000));
+        const pricePerDay = await getGamePricePerDayById(gameId)
 
-    let delayFee = 0;
+        let daysDiff = calculateDaysDiff(rentDate, daysRented, returnDate)
 
-    if (diffInDays > rental.daysRented) {
+        const delayFee = pricePerDay * daysDiff
 
-      const addicionalDays = diffInDays - rental.daysRented;
-      delayFee = addicionalDays * rental.originalPrice;
-      
+        await db.query(
+            `UPDATE rentals SET "delayFee" = $1, "returnDate" = $2 
+            WHERE id = $3`,
+            [delayFee, returnDate, id]
+        )
+        return res.sendStatus(200)
+
+    } catch (err) {
+        console.log(err)
+        return res.sendStatus(500)
     }
-
-    const daysDiff =  Math.floor(delayFee / (24 * 3600 * 1000));
-
-    await connectionDB.query(
-      `
-        UPDATE rentals
-        SET "returnDate" = NOW(), "delayFee" = $1
-        WHERE id=$2
-     `,
-      [delayFee, id]
-    );
-
-    res.sendStatus(200);
-  } catch (err) {
-    res.status(500).send(err.message);
-  }
 }
 
 
